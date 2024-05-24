15f0d24314542fbff8aab7d27ec1d1ea2f400647
Jhon Guerrero <garruxx@gmail.com>
2024-05-24 01:31:07 -0500

    fixed(install) :bug: sh validations problems resolve

--- fast-install.sh
+++ fast-install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-echo -e "\e[35m Welcome to Pinku Theme installation\e[0m"
+echo "\e[35m Welcome to Pinku Theme installation\e[0m"
 wget https://pinku.igarrux.com/pinku-theme.omp.json -qO pinku-theme.omp.json
 
 # check if oh-my-posh is installed
 echo "Verifying that oh-my-posh is installed"
 if command -v "oh-my-posh" &>/dev/null; then
-    echo -e "\e[96moh-my-posh ✅\e[0m"
+    echo "\e[96moh-my-posh ✅\e[0m"
 else
-    echo -e "\e[33moh-my-posh is not installed, installing...\e[0m"
+    echo "\e[33moh-my-posh is not installed, installing...\e[0m"
     echo "executing oh-my-posh installation"
     curl -s https://ohmyposh.dev/install.sh | bash -s
 
@@ -19,24 +19,20 @@ mkdir -p ~/.oh-my-posh/themes
 mv ./pinku-theme.omp.json ~/.oh-my-posh/themes
 
 # Config oh-my-posh ant theme
-echo -e "\e[35mConfiguring the theme...\e[0m"
-
-if [ "$0" == *"bash"* ]; then
+echo "\e[35mConfiguring the theme...\e[0m"
+case "$0" in
+*"bash"*)
     echo "eval \"\$(oh-my-posh init bash --config '~/.oh-my-posh/themes/pinku-theme.omp.json')\"" >>~/.profile
     echo "eval \"\$(oh-my-posh init bash --config '~/.oh-my-posh/themes/pinku-theme.omp.json')\"" >>~/.bashrc
-    eval "$(oh-my-posh init bash --config '~/.oh-my-posh/themes/pinku-theme.omp.json')"
-
-elif [ "$0" == *"zsh"* ]; then
+    ;;
+*"zsh"*)
     echo "eval \"\$(oh-my-posh init zsh --config '~/.oh-my-posh/themes/pinku-theme.omp.json')\"" >>~/.zshrc
-    eval "$(oh-my-posh init zsh --config '~/.oh-my-posh/themes/pinku-theme.omp.json')"
-
-else
+    ;;
+*)
     echo "i love fish"
     echo "oh-my-posh init fish --config ~/.oh-my-posh/themes/pinku-theme.omp.json | source" >>~/.config/fish/config.fish
-    eval "$(oh-my-posh init fish --config '~/.oh-my-posh/themes/pinku-theme.omp.json')"
-fi
+    ;;
+esac
 
-echo -e "\e[35m🎉 Congratulations! You already have oh-my-posh and the pinku theme. \e[0m"
+echo "\e[35m🎉 Congratulations! You already have oh-my-posh and the pinku theme. \e[0m"
 echo "now just configure the font in your terminal, you can see a tutorial at https://configs.igarrux.com/terminal-font"
-exit 
-exit
